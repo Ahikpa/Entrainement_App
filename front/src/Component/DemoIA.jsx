78f5e51dc7f7c1e4fc7bc2ac10434ff1043c02f7
axel.karamoko <axel.karamoko@inexa-ci.com>
2023-01-15 22:49:09 +0000

    ajout du websocket

--- front/src/Component/DemoIA.jsx
+++ front/src/Component/DemoIA.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReconnectingWebSocket from "reconnecting-websocket";
+import _ from "lodash";
 const socketUrl = "ws://127.0.0.1:8080";
 
-async function initApp() {
-  const rws = new ReconnectingWebSocket(socketUrl);
-  rws.addEventListener("open", () => {
-    rws.send("hello!");
-  });
-
-  rws.addEventListener("message", (message) => {
-    console.log("new message =>", message);
-  });
-}
-
 function DemoIA() {
+  const [data, setData] = useState({
+    available: 0,
+    total: 0,
+  });
+
+  async function initApp() {
+    const rws = new ReconnectingWebSocket(socketUrl);
+    rws.addEventListener("message", (message) => {
+      try {
+        const jsonData = JSON.parse(message.data);
+        if (!_.isEqual(jsonData, data.current)) setData(jsonData);
+      } catch (error) {}
+    });
+  }
+
   useEffect(() => {
     initApp();
   }, []);
 
   return (
     <div>
-      <h1>Hey</h1>
+      <h1>Données sur les places</h1>
+      <ul>
+        <li>Nombre de places disponibles: {data.available}</li>
+        <li>Nombre total de places: {data.total}</li>
+      </ul>
     </div>
   );
 }
--- server-socket/index.js
+++ server-socket/index.js
@@ -4,32 +4,39 @@ const { readFileSync } = require("fs");
 const { WebSocketServer } = require("ws");
 
 const port = 8080;
+const redisPort = 6379;
+const redisHost = "0.0.0.0";
 const redisChannel = "ai_broker";
-const redisClient = createClient({ host: "127.0.0.1", port: 6379 });
-
 const server = createServer();
 const ws = new WebSocketServer({ server });
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
 
-async function startRedisServer() {
-  const subscriber = redisClient.duplicate();
-  await subscriber.connect();
-  await subscriber.subscribe(redisChannel, (message) => {
-    console.log(message);
+const createRedisClient = async () => {
+  const redisClient = createClient({
+    socket: {
+      port: redisPort,
+      host: redisHost,
+    },
+  });
+
+  await redisClient.connect();
+
+  await redisClient.subscribe(redisChannel, function (message) {
+    eventEmitter.emit(redisChannel, message);
+  });
+};
+
+ws.on("connection", (ws) => {
+  eventEmitter.on(redisChannel, (message) => {
     ws.send(message);
   });
-  console.log("connected");
-}
-
-ws.on("connection", function connection(ws) {
-  console.log("new client connected");
   // ws.on("message", function message(data) {
   //   console.log("received: %s", data);
   // });
-  //
 });
 
-startRedisServer();
-
-server.listen(port, () => {
+server.listen(port, async () => {
+  await createRedisClient();
   console.log(`listening on port ${port}`);
 });
